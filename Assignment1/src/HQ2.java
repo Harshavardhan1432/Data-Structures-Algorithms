7b4b5820e14e4e00d4cd6595c8ebfac98d745ebd
Harshavardhan <harshavardhanponduru@gmail.com>
2024-03-14 02:04:08 +0530

    Add files via upload

--- Assignment1/src/HQ2.java
+++ Assignment1/src/HQ2.java
@@ -4,10 +4,10 @@ public class HQ2 {
         Scanner hv = new Scanner(System.in);
         System.out.println("Enter the number of lines");
         int n = hv.nextInt();
-        String [] str = new String[n];
-        for (int i = 0; i < n; i++)
+        String [] str = new String[n+1];
+        for (int i = 0; i <= n; i++)
             str[i] = hv.nextLine();
-        for (int j = n-1; j >= 0; j--)
+        for (int j = n; j >= 0; j--)
             System.out.println(str[j]);
     }
 }
