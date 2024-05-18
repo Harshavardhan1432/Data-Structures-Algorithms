c1f0d2d48faf07bc2e548d73a8a76bc0d2136955
Harshavardhan <70995423+Harshavardhan1432@users.noreply.github.com>
2024-05-18 12:51:00 +0530

    Add files via upload
    Signed-off-by: Harshavardhan  <70995423+Harshavardhan1432@users.noreply.github.com>

---
+++ Assignment 4/src/Node.java
@@ -0,0 +1,1 @@
+public class Node {    int regNo;    float marks;    Node next;    Node(int regNo, float marks) {        this.regNo = regNo;        this.marks = marks;        this.next = null;    }}
